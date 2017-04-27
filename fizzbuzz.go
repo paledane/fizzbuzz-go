88d49fb8d33409eab5debeaf64713ce523b2dc76
Palle Andersen <palle.andersen.dk@gmail.com>
2017-04-27 22:35:33 +0200

    Formatted comments

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -1,16 +1,16 @@
 package fizzbuzz
 
-//IsFizz returns true if value is divisable by 3
+// IsFizz returns true if value is divisable by 3
 func IsFizz(value int) bool {
 	return value%3 == 0
 }
 
-//IsBuzz returns true if value is divisable by 5
+// IsBuzz returns true if value is divisable by 5
 func IsBuzz(value int) bool {
 	return value%5 == 0
 }
 
-//IsFizzBuzz returns true if value is divisable by 3 and 5
+// IsFizzBuzz returns true if value is divisable by 3 and 5
 func IsFizzBuzz(value int) bool {
 	return value%15 == 0
 }
