d8b3999dae45e2d01e0462edada73eb40a2d3309
Palle Andersen <palle.andersen.dk@gmail.com>
2017-04-30 19:46:01 +0200

    Added a test with a prime number

--- fizzbuzz/fizzbuzz.go
+++ fizzbuzz/fizzbuzz.go
@@ -1,9 +1,13 @@
 package fizzbuzz
 
+import (
+	"strconv"
+)
+
 // Calc returns the specified number. But for multiples of three
 // “Fizz” is returned instead of the number and for the multiples
 // of five print “Buzz” is returned. For numbers which are multiples
 // of both three and five “FizzBuzz” is returned.
 func Calc(i int) string {
-	return "1"
+	return strconv.Itoa(i)
 }
--- fizzbuzz/fizzbuzz_test.go
+++ fizzbuzz/fizzbuzz_test.go
@@ -12,3 +12,14 @@ func Test1Is1(t *testing.T) {
 		t.Errorf("Calc(%d) == %v, want %v", in, result, want)
 	}
 }
+
+func Test13Is13(t *testing.T) {
+	want := "13"
+	in := 13
+
+	result := Calc(in)
+
+	if result != want {
+		t.Errorf("Calc(%d) == %v, want %v", in, result, want)
+	}
+}
