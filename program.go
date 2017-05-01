f1139af66de45d282f74beaf5e87f01a83b8bc2a
Palle Andersen <palle.andersen.dk@gmail.com>
2017-05-01 21:19:29 +0200

    Use fizzbuzz package

--- program.go
+++ program.go
@@ -2,20 +2,14 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/paledane/fizzbuzz-go/fizzbuzz"
 )
 
 func main() {
 
 	for i := 1; i <= 100; i++ {
-		if i%15 == 0 {
-			fmt.Printf("FizzBuzz\n")
-		} else if i%3 == 0 {
-			fmt.Printf("Fizz\n")
-		} else if i%5 == 0 {
-			fmt.Printf("Buzz\n")
-		} else {
-			fmt.Printf("%d\n", i)
-		}
+		fmt.Printf("%q\n", fizzbuzz.Calc(i))
 	}
 
 }
