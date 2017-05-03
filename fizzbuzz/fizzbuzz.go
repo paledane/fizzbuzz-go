c7421f12d6927f34aedcceeaff10e089e81b4a6a
Palle Andersen <palle.andersen.dk@gmail.com>
2017-05-03 20:48:00 +0200

    Added FizzHandler as a closure

--- fizzbuzz/fizzbuzz.go
+++ fizzbuzz/fizzbuzz.go
@@ -7,3 +7,12 @@ func DefaultHandler(next func(int) string) func(int) string {
 		return strconv.Itoa(i)
 	}
 }
+
+func FizzHandler(next func(int) string) func(int) string {
+	return func(i int) string {
+		if i%3 == 0 {
+			return "Fizz"
+		}
+		return strconv.Itoa(i)
+	}
+}
--- fizzbuzz/fizzbuzz_test.go
+++ fizzbuzz/fizzbuzz_test.go
@@ -8,6 +8,16 @@ func TestDefaultHandler(t *testing.T) {
 	want := "1"
 
 	if result := sut(in); result != want {
-		t.Errorf("Calc(%d) == %q, want %q", in, result, want)
+		t.Errorf("func(%d) == %q, want %q", in, result, want)
+	}
+}
+
+func TestFizzHandler(t *testing.T) {
+	sut := FizzHandler(nil)
+	in := 3
+	want := "Fizz"
+
+	if result := sut(in); result != want {
+		t.Errorf("func(%d) == %q, want %q", in, result, want)
 	}
 }
